bb176b4d43e5c60f01b234a8d1cae2ef534e402c
hiddify <hiddify@gmail.com>
2023-03-05 19:38:17 +0100

    new: add new icon :-)

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -2,10 +2,12 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "@/locales/en.json";
 import zh from "@/locales/zh.json";
+import fa from "@/locales/fa.json";
 
 const resources = {
   en: { translation: en },
   zh: { translation: zh },
+  fa: { translation: fa },
 };
 
 i18n.use(initReactI18next).init({
